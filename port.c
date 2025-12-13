a367654a074d8f56f6381324f7422b7ddc7b0fc5
ezequiel_ayala <ezequielma21@gmail.com>
2025-12-13 17:43:00 -0600

    feat: add llibrary

---
+++ port.c
@@ -0,0 +1,10 @@
+#include "port.h"
+//comments
+
+/* Función básica: inicializa un LED y lo hace parpadear */
+void LED_Init(void)
+{
+    /* Habilita el reloj del puerto F */
+    /* Espera a que el periférico esté listo */
+    /* Configura PF1 (LED rojo) como salida */
+}
